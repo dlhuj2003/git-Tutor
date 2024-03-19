0b77e18d246427710206e1dff01713932960c4e7
dluhj2003 <obinnaedmund39@gmail.com>
2024-03-19 14:55:34 -0700

    added new contents to  the js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// let iceCream = "Chocolate";
+// if (iceCream === "chocolate") {
+//   alert("Yay, i love chocolate icecream");
+// }
+// else {
+//   alert("Awwn! chocolate ice cream is my favorite");
+// }
